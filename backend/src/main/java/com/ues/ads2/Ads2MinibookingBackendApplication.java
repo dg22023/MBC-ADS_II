4be46908284dc1b13e6aec992521379b6155be00
Gabriel Diaz <dg22023@ues.edu.sv>
2025-09-12 17:08:03 -0600

    Se agrego el inicio de spring/backend

---
+++ backend/src/main/java/com/ues/ads2/Ads2MinibookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ues.ads2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ads2MinibookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ads2MinibookingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ues/ads2/Ads2MinibookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ues.ads2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ads2MinibookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
