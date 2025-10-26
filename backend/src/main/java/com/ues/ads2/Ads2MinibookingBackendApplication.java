7f6bc4faa0196f885fa30042468ebbacf44d40cd
gabus2194 <dg22023@ues.edu.sv>
2025-10-26 14:54:27 -0600

    fixhot:Solucion a problema de yaml en docker y creacion de nginx

--- backend/src/main/java/com/ues/ads2/Ads2MinibookingBackendApplication.java
+++ backend/src/main/java/com/ues/ads2/Ads2MinibookingBackendApplication.java
@@ -3,20 +3,15 @@ package com.ues.ads2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class Ads2MinibookingBackendApplication {
-	public static void main(String[] args) {
-	 
-		//Cargando vaiables de entorno en .env
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		System.setProperty("JDBC_DATABASE_URL", dotenv.get("JDBC_DATABASE_URL"));
-        	System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        	System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-        	
-		SpringApplication.run(Ads2MinibookingBackendApplication.class, args);
-		
-	}
-
+    public static void main(String[] args) {
+     
+        // Elimina todo el código de dotenv.
+        // Spring Boot leerá automáticamente las variables
+        // SPRING_DATASOURCE_URL, SPRING_DATASOURCE_USERNAME, y SPRING_DATASOURCE_PASSWORD
+        // que Docker Compose le está pasando.
+            
+        SpringApplication.run(Ads2MinibookingBackendApplication.class, args);
+    }
 }
